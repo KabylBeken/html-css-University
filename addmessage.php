fbc5c7aba0f44089fc4b1681e621b9807bf9cf31
BekenKabyl <legionvtvtv@gmail.com>
2024-06-03 20:17:31 +0500

    Your commit message

---
+++ addmessage.php
@@ -0,0 +1,26 @@
+<?php
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=message", 'root','');
+
+    if(empty($_POST['name'])) exit("Поле не заполнено");
+    if(empty($_POST['content'])) exit("Поле не заполнено");
+
+    $query = "INSERT INTO message VALUES(NULL, :name, NOW())";
+    $msg = $conn->prepare($query);
+    $msg->execute(['name'=> $_POST['name']]);
+
+    $msg_id = $conn->lastInsertId();
+
+    $query = "INSERT INTO message content VALUES(NULL, :content, :message_id)";
+    $msg = $conn->prepare($query);
+    $msg->execute(['content' => $_POST['content'], 'message_id' => $msg_id]);
+
+    header("Location: index.html");
+
+}
+catch (PDOException $e)
+{
+    echo "error" .$e->getMessage();
+}
+
+?>
